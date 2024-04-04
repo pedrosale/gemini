9cb0e155e32a73196bfe428b4d9903995e8adfb2
pedrosale <53319245+pedrosale@users.noreply.github.com>
2024-04-04 18:42:21 -0300

    Update app.py

--- app.py
+++ app.py
@@ -98,6 +98,9 @@ def main():
     initialize_session_state()
     st.title('Chatbot Assistente com Gemini')
     # URL direta para a imagem hospedada no GitHub
+    image_url = 'https://raw.githubusercontent.com/pedrosale/falcon_test/af8a20607bae402a893817be0a766ec55a9bcec3/RAG2.jpg'
+    # Exibir a imagem usando a URL direta
+    st.image(image_url, caption='Arquitetura atual: GitHub + Streamlit')
     st.markdown('**Esta versão contém:**  \nA) Gemini ⌘ [gemini-pro](https://blog.google/intl/pt-br/novidades/nosso-modelo-de-proxima-geracao-gemini-15/);  \nB) Conjunto de dados pré-carregados do CTB [1. Arquivo de Contexto](https://raw.githubusercontent.com/pedrosale/falcon_test/main/CTB3.txt) e [2. Reforço de Contexto](https://raw.githubusercontent.com/pedrosale/falcon_test/main/CTB2.txt);  \nC) ["Retrieval Augmented Generation"](https://python.langchain.com/docs/use_cases/question_answering/) a partir dos dados carregados (em B.).')
     
     chain = create_conversational_chain()
