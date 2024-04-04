0d14c23b60dbf46b5cb644c4e56cf7b58704f18a
pedrosale <53319245+pedrosale@users.noreply.github.com>
2024-04-04 18:09:20 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 # Import necessary libraries
-import os
 import streamlit as st
-import google.generativeai as genai 
+from streamlit_chat import message
+import google.generativeai as genai
 from dotenv import load_dotenv
+import os
+import tempfile
+import urllib.request
 from PIL import Image
 
 # Load environment variables from .env file
@@ -14,89 +17,58 @@ api_key = os.getenv("GOOGLE_API_KEY")
 # Configure the Google Generative AI with the API key
 genai.configure(api_key=api_key)
 
-# Set the page configuration for the Streamlit app
-st.set_page_config(
-    page_title="Google Gemini Models",
-    page_icon="🤖"
-)
+def initialize_session_state():
+    if 'history' not in st.session_state:
+        st.session_state['history'] = []
 
-# Check if the Google API key is provided in the sidebar
-with st.sidebar:
-    if 'GOOGLE_API_KEY' in st.secrets:
-        st.success('API key already provided!', icon='✅')
-        api_key = st.secrets['GOOGLE_API_KEY']
-    else:
-        api_key = st.text_input('Enter Google API Key:', type='password')
-        if not (api_key.startswith('AI')):
-            st.warning('Please enter your API Key!', icon='⚠️')
-        else:
-            st.success('Success!', icon='✅')
-    os.environ['GOOGLE_API_KEY'] = api_key
-    "[Get a Google Gemini API key](https://ai.google.dev/)"
-    "[View the source code](https://github.com/wms31/streamlit-gemini/blob/main/app.py)"
-    "[Check out the blog post!](https://letsaiml.com/creating-google-gemini-app-with-streamlit/)"
+    if 'generated' not in st.session_state:
+        st.session_state['generated'] = ["Como posso te ajudar?"]
 
-# Set the title and caption for the Streamlit app
-st.title("🤖 Google Gemini Models")
-st.caption("🚀 A streamlit app powered by Google Gemini")
+    if 'past' not in st.session_state:
+        st.session_state['past'] = ["Olá, sou seu assistente."]
 
-# Create tabs for the Streamlit app
-tab1, tab2 = st.tabs(["🌏 Generate Travel Plans - Gemini Pro", "🖼️ Visual Venture - Gemini Pro Vision"])
-
-# Code for Gemini Pro model
-with tab1:
-    st.write("💬 Using Gemini Pro - Text only model")
-    st.subheader("🌏 Generate travel itineraries")
-    
-    destination_name = st.text_input("Enter destination name: \n\n",key="destination_name",value="United Arab Emirates")
-    days = st.text_input("How many days would you like the itinerary to be? \n\n",key="days",value="5")
-    suggested_attraction = st.text_input("What should the first suggested attraction be for the trip? \n\n",key="suggested_attraction",value="Visiting Burj Khalifa in Dubai.")
-        
-    prompt = f"""Come up with a {days}-day itinerary for a trip to {destination_name}. The first suggested attraction should be {suggested_attraction}
+def conversation_chat(query, history):
+    prompt = """
+    Você é um assistente que só conversa no idioma português do Brasil (você nunca, jamais conversa em outro idioma que não seja o português do Brasil).
+    Você responde as perguntas do usuário com base nos arquivos carregados.
+    Vamos pensar passo a passo para responder.
     """
+    query_with_prompt = prompt + query
+    model = genai.GenerativeModel("gemini-pro", generation_config={"temperature": 0.7, "max_output_tokens": 512})
+    result = model.generate_content(query_with_prompt)
+    history.append((query, result.text))
+    return result.text
+
+def display_chat_history():
+    reply_container = st.container()
+    container = st.container()
+
+    with container:
+        with st.form(key='my_form', clear_on_submit=True):
+            user_input = st.text_input("question:", placeholder="Me pergunte sobre o(s) conjunto(s) de dados pré-carregados", key='input')
+            submit_button = st.form_submit_button(label='Enviar')
+
+        if submit_button and user_input:
+            with st.spinner('Generating response...'):
+                output = conversation_chat(user_input, st.session_state['history'])
+
+            st.session_state['past'].append(user_input)
+            st.session_state['generated'].append(output)
+
+    if st.session_state['generated']:
+        with reply_container:
+            for i in range(len(st.session_state['generated'])):
+                logo_url = 'https://your_logo_url_here.png'
+                message(st.session_state["past"][i], is_user=True, key=str(i) + '_user', logo=logo_url)
+                message(st.session_state["generated"][i], key=str(i), logo=logo_url)
+
+def main():
+    # Initialize session state
+    initialize_session_state()
+    st.title('🦅💬 Chatbot Assistente com Gemini.')
     
-    config = {
-        "temperature": 0.8,
-        "max_output_tokens": 2048,
-        }
-    
-    generate_t2t = st.button("Generate my travel itinerary", key="generate_t2t")
-    model = genai.GenerativeModel("gemini-pro", generation_config=config)
-    if generate_t2t and prompt:
-        with st.spinner("Generating your travel itinerary using Gemini..."):
-            plan_tab, prompt_tab = st.tabs(["Travel Itinerary", "Prompt"])
-            with plan_tab: 
-                response = model.generate_content(prompt)
-                if response:
-                    st.write("Your plan:")
-                    st.write(response.text)
-            with prompt_tab: 
-                st.text(prompt)
+    # Display the chat history and the input form for new questions
+    display_chat_history()
 
-# Code for Gemini Pro Vision model
-with tab2:
-    st.write("🖼️ Using Gemini Pro Vision - Multimodal model")
-    st.subheader("🔮 Generate image to text responses")
-    
-    image_prompt = st.text_input("Ask any question about the image", placeholder="Prompt", label_visibility="visible", key="image_prompt")
-    uploaded_file = st.file_uploader("Choose an image", type=["jpg", "jpeg", "png"])
-    image = ""
-
-    if uploaded_file is not None:
-        image = Image.open(uploaded_file)
-        st.image(image, caption="Uploaded Image.", use_column_width=True)
-
-    submit=st.button("Generate Response")
-
-    if submit:
-        model = genai.GenerativeModel('gemini-pro-vision')
-        with st.spinner("Generating your response using Gemini..."):
-            if image_prompt!="":
-                response = model.generate_content([image_prompt,image])
-            else:
-                response = model.generate_content(image)
-        response = response.text
-        st.subheader("Gemini's response")
-        st.write(response)
-
-    
+if __name__ == "__main__":
+    main()
