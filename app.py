e58ced8bac8f4118bd0ce25b3fd3b09a19a07cbf
pedrosale <53319245+pedrosale@users.noreply.github.com>
2024-04-05 09:42:37 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,6 @@
+__import__('pysqlite3')
+import sys
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
 import streamlit as st
 from langchain.prompts import PromptTemplate
 from langchain.chains.question_answering import load_qa_chain
